1b783fa332cee01881da7892650aa7909dcb2313
lizhnekui <lizhenkui@qian88.com.cn>
2019-01-29 15:14:04 +0800

    快三过滤

--- bc-wap/bc-wap/target/classes/static/js/filter/k3Filter.js
+++ bc-wap/bc-wap/target/classes/static/js/filter/k3Filter.js
@@ -1,260 +1,312 @@
-document.write("<script type='text/javascript' src='/js/filter/k3Num.js'></script>");
-document.write("<script type='text/javascript' src='/js/filter/k3Util.js'></script>");
+document
+		.write("<script type='text/javascript' src='/js/filter/k3Num.js'></script>");
+document
+		.write("<script type='text/javascript' src='/js/filter/k3Util.js'></script>");
 
 // 0未选中、1选中、2不可选
 // 胆码选择情况
-var danChoiceList = [0,0,0,0,0,0];
+var danChoiceList = [ 0, 0, 0, 0, 0, 0 ];
 // 托码选择情况
-var tuoChoiceList = [0,0,0,0,0,0];
+var tuoChoiceList = [ 0, 0, 0, 0, 0, 0 ];
 // 类型选择情况
-var typeChoiceList = [2,2,2,2];
-//和值选择情况
-var sumChoiceList = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2];
-//奇偶选择情况
-var odd_evenChoiceList = [2,2,2,2];
+var typeChoiceList = [ 2, 2, 2, 2 ];
+// 和值选择情况
+var sumChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+// 奇偶选择情况
+var odd_evenChoiceList = [ 2, 2, 2, 2 ];
 // 跨度选择情况
-var spanChoiceList = [2,2,2,2,2,2];
-//012选择情况
-var lyeChoiceList = [2,2,2,2,2,2,2,2,2,2];
-//大中小选择情况
-var dzxChoiceList = [2,2,2,2,2,2,2,2,2,2];
+var spanChoiceList = [ 2, 2, 2, 2, 2, 2 ];
+// 012选择情况
+var lyeChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+// 大中小选择情况
+var dzxChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
 // 组出开奖号码
 var k3NumList = [];
+// 号码选择情况 -1 未选择 其他值对应数组下标。 第一位代表类型、第二位代表和值、以此类推跨度、奇偶、012、大中小
+var choiceDetail = [ -1, -1, -1, -1, -1, -1 ];
 
-function choiceAction(){
+function choiceAction() {
 	K3CalDanTuoNum();
-	filterNum();
 	calOtherFilter();
 	drawPageAll();
 	printResult();
 }
 // 选择胆码
-function choiceDan(index){
+function choiceDan(index) {
 	var danStatus = danChoiceList[index];
-	if(danStatus == 0){
+	if (danStatus == 0) {
 		danChoiceList[index] = 1;
 		tuoChoiceList[index] = 2;
-	}else if(danStatus == 1){
+	} else if (danStatus == 1) {
 		danChoiceList[index] = 0;
 		tuoChoiceList[index] = 0;
 	}
+	initChoiceList();
 	choiceAction();
 }
 // 选择托码
-function choiceTuo(index){
+function choiceTuo(index) {
 	var tuoStatus = tuoChoiceList[index];
-	if(tuoStatus == 0){
+	if (tuoStatus == 0) {
 		danChoiceList[index] = 2;
 		tuoChoiceList[index] = 1;
-	}else if(tuoStatus == 1){
+	} else if (tuoStatus == 1) {
 		danChoiceList[index] = 0;
 		tuoChoiceList[index] = 0;
 	}
+	initChoiceList();
 	choiceAction();
 }
 // 选择类型、跨度、和值......
-function choiceOther(index,type){
+function choiceOther(index, type) {
 	var typeStatus = 0;
 	var choiceList = [];
-	if(type == "type"){
+	var choiceDetailIndex = 0;// 选择详情数组下标
+
+	if (type == "type") {// 类型
+		choiceDetailIndex = 0;
 		choiceList = typeChoiceList;
-	}else if(type == "span"){
+	} else if (type == "hz") {// 和值
+		choiceDetailIndex = 1;
+		choiceList = sumChoiceList;
+	} else if (type == "span") {// 跨度
+		choiceDetailIndex = 2;
 		choiceList = spanChoiceList;
+	} else if (type == "jo") {// 奇偶
+		choiceDetailIndex = 3;
+		choiceList = odd_evenChoiceList;
+	} else if (type == "012") {// 012
+		choiceDetailIndex = 4;
+		choiceList = lyeChoiceList;
+	} else if (type == "dzx") {// 大中小
+		choiceDetailIndex = 5;
+		choiceList = dzxChoiceList;
 	}
-	
+
 	var typeStatus = choiceList[index];
-	
-	if(typeStatus == 0){
+
+	if (typeStatus == 0) {// 选中
 		choiceList[index] = 1;
-	}else if(typeStatus == 1){
+		choiceDetail[choiceDetailIndex] = index;
+	} else if (typeStatus == 1) {// 取消选择
+		choiceDetail[choiceDetailIndex] = -1;
 		choiceList[index] = 0;
 	}
 	choiceAction();
 }
 
-function printResult(){
-	for(var i=0;i<k3NumList.length;i++){
-		console.log(k3NumList[i].one + " " + k3NumList[i].two +  " " + k3NumList[i].three);
+function printResult() {
+	for (var i = 0; i < k3NumList.length; i++) {
+		console.log(k3NumList[i].one + " " + k3NumList[i].two + " "
+				+ k3NumList[i].three);
 	}
 }
-
-
 
 /**
  * 胆拖计算选择号码
  */
-function K3CalDanTuoNum(){
+function K3CalDanTuoNum() {
 	k3NumList.length = 0;
 	var dan = [];
 	var tuo = [];
-	
-// List<K3Num> k3NumList = new ArrayList<K3Num>();
-	
-	for (var i=0;i<danChoiceList.length;i++)
-	{
-		if(danChoiceList[i] == 1){
-			dan.push(i+1);
+
+	// List<K3Num> k3NumList = new ArrayList<K3Num>();
+
+	for (var i = 0; i < danChoiceList.length; i++) {
+		if (danChoiceList[i] == 1) {
+			dan.push(i + 1);
 		}
 	}
-	
-	for (var i=0;i<tuoChoiceList.length;i++)
-	{
-		if(tuoChoiceList[i] == 1){
-			tuo.push(i+1);
+
+	for (var i = 0; i < tuoChoiceList.length; i++) {
+		if (tuoChoiceList[i] == 1) {
+			tuo.push(i + 1);
 		}
 	}
-	
-	if(dan.length != 0){
+
+	if (dan.length != 0) {
 		K3CalDanNum(dan);
 	}
-	
-	
+
 	// 全托情况
-	if(!dan || dan.length == 0){
-		for(var i=0;i<tuo.length;i++){// 豹子
+	if (!dan || dan.length == 0) {
+		for (var i = 0; i < tuo.length; i++) {// 豹子
 			var tuoList = [];
 			tuoList.push(tuo[i]);
 			K3CalDanNum(tuoList);
 		}
-		
-		for(var i=0;i<tuo.length;i++){
-			for(var j=i+1;j<tuo.length;j++){
+
+		for (var i = 0; i < tuo.length; i++) {
+			for (var j = i + 1; j < tuo.length; j++) {
 				var tuoList = [];
 				tuoList.push(tuo[i]);
 				tuoList.push(tuo[j]);
 				K3CalDanNum(tuoList);
 			}
 		}
-		
-		for(var i=0;i<tuo.length;i++){
-			for(var j=i+1;j<tuo.length;j++){
-				for(var k=j+1;k<tuo.length;k++){
-					var k3Num = new K3Num(tuo[i],tuo[j],tuo[k]);
+
+		for (var i = 0; i < tuo.length; i++) {
+			for (var j = i + 1; j < tuo.length; j++) {
+				for (var k = j + 1; k < tuo.length; k++) {
+					var k3Num = new K3Num(tuo[i], tuo[j], tuo[k]);
 					k3NumList.push(k3Num);
 				}
-				
+
 			}
 		}
 	}
-	
-	
-	if(dan.length == 1){// 两个及以上托码情况
-		for(var i=0;i<tuo.length;i++){
-			var k3Num = new K3Num(dan[0],tuo[i],tuo[i]);
+
+	if (dan.length == 1) {// 两个及以上托码情况
+		for (var i = 0; i < tuo.length; i++) {
+			var k3Num = new K3Num(dan[0], tuo[i], tuo[i]);
 			k3NumList.push(k3Num);
-			for(var j=i+1;j<tuo.length;j++){
-				var k3Num = new K3Num(dan[0],tuo[i],tuo[j]);
+			for (var j = i + 1; j < tuo.length; j++) {
+				var k3Num = new K3Num(dan[0], tuo[i], tuo[j]);
 				k3NumList.push(k3Num);
 			}
 		}
 		dan.push(dan[0]);
-	}else if(dan.length == 1){
+	} else if (dan.length == 1) {
 		dan.push(dan[0]);
 	}
-	
-	
-	if(dan.length == 2){
-		for(var i=0;i<tuo.length;i++){
-			var k3Num = new K3Num(dan[0],dan[1],tuo[i]);
+
+	if (dan.length == 2) {
+		for (var i = 0; i < tuo.length; i++) {
+			var k3Num = new K3Num(dan[0], dan[1], tuo[i]);
 			k3NumList.push(k3Num);
 		}
-	}else if(dan.length == 3){
-			var k3Num = new K3Num(dan[0],dan[1],dan[2]);
-			k3NumList.push(k3Num);
+	} else if (dan.length == 3) {
+		var k3Num = new K3Num(dan[0], dan[1], dan[2]);
+		k3NumList.push(k3Num);
 	}
 }
-
 
 /**
  * 全胆号码组合情况
  */
-function K3CalDanNum(dan){
-	
-	if(dan.length == 1){
-		var k3Num = new K3Num(dan[0],dan[0],dan[0]);
+function K3CalDanNum(dan) {
+
+	if (dan.length == 1) {
+		var k3Num = new K3Num(dan[0], dan[0], dan[0]);
 		k3NumList.push(k3Num);
-	}else if(dan.length == 2){
-		var k3Num = new K3Num(dan[0],dan[0],dan[1]);
+	} else if (dan.length == 2) {
+		var k3Num = new K3Num(dan[0], dan[0], dan[1]);
 		k3NumList.push(k3Num);
-		
-		var k3Num1 = new K3Num(dan[0],dan[1],dan[1]);
+
+		var k3Num1 = new K3Num(dan[0], dan[1], dan[1]);
 		k3NumList.push(k3Num1);
 	}
 }
-function filterNum(){
-	
-}
-
-// //////////////////////////////计算形态
-function calOtherFilter(){
-	var typeChoiceListTemp = [2,2,2,2];
-	// 计算与过滤类型
-	calculateOtherType(typeChoiceListTemp);
-	
-	setChoiceStatus();
-	
-	if(k3NumList.length == 0){
-		initChoiceList(typeChoiceList,typeChoiceListTemp);
-	}
-	
-}
-//设置号码选择情况
-function setChoiceStatus(choiceList,choiceListTemp){
-	for(var i=0;i<choiceListTemp.length;i++){
-		if(choiceListTemp[i] == 2){
-			choiceList[i] = 2;
-		}else if(choiceListTemp[i] == 0){
-			if(choiceList[i] == 2){
-			   choiceList[i] = 0;
-			} 
-		}
-	}
-}
-function initChoiceList(){
-	typeChoiceList = [2,2,2,2];
-	spanChoiceList = [2,2,2,2,2,2];
+function initChoiceList() {
+	// 类型选择情况
+	typeChoiceList = [ 2, 2, 2, 2 ];
+	// 和值选择情况
+	sumChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+	// 奇偶选择情况
+	odd_evenChoiceList = [ 2, 2, 2, 2 ];
+	// 跨度选择情况
+	spanChoiceList = [ 2, 2, 2, 2, 2, 2 ];
+	// 012选择情况
+	lyeChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+	// 大中小选择情况
+	dzxChoiceList = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+	// 组出开奖号码
+	choiceDetail = [ -1, -1, -1, -1, -1, -1 ];
 }
 /**
  * 根据号码计算形态
- * 
- * @param k3Num
- * @return
  */
-function calculateOtherType(typeChoiceListTemp){
-	for(var i=0;i<k3NumList.length;i++){
+function calOtherFilter() {
+	var typeChoiceListTemp = [ 2, 2, 2, 2 ];// 类型
+
+	var sumChoiceListTemp = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,2, 2 ];// 和值选择情况
+	// 跨度选择情况
+	var spanChoiceListTemp = [ 2, 2, 2, 2, 2, 2 ];
+	// 奇偶选择情况
+	var odd_evenChoiceListTemp = [ 2, 2, 2, 2 ];
+	// 012选择情况
+	var lyeChoiceListTemp = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+	// 大中小选择情况
+	var dzxChoiceListTemp = [ 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ];
+
+	var filterCondition = [[typeChoiceListTemp,0,calculateType,typeChoiceList],[sumChoiceListTemp,1,calculateSum,sumChoiceList],[spanChoiceListTemp,2,calculateSpan,spanChoiceList]
+							,[odd_evenChoiceListTemp,3,calculateOddEven,odd_evenChoiceList],[lyeChoiceListTemp,4,calculate012,lyeChoiceList],[dzxChoiceListTemp,5,calculateBigMidSmall,dzxChoiceList]];
+	for (var i = 0; i < k3NumList.length; i++) {
 		var k3Num = k3NumList[i]
-		var result = calculateType(k3Num,typeChoiceListTemp);
-		typeChoiceListTemp[result] = 0;
+		for(var j=0;j<filterCondition.length;j++){
+			var typeResult = filterCondition[j][2](k3Num, filterCondition[j][0]);
+			if (!removeK3Num(typeResult, filterCondition[j][0], i,filterCondition[j][1])) {
+				i--;
+				break;
+			}
+		}
 	}
-	
+	//根据过滤后的号码计算号码形态
+	for (var i = 0; i < k3NumList.length; i++) {
+		var k3Num = k3NumList[i]
+		for(var j=0;j<filterCondition.length;j++){
+			var typeResult = filterCondition[j][2](k3Num, filterCondition[j][0]);
+			filterCondition[j][0][typeResult] = 0;
+		}
+	}
+	for(var j=0;j<filterCondition.length;j++){
+		setChoiceStatus(filterCondition[j][3], filterCondition[j][0]);
+	}
+
+	// 无任何号码，初始化数据
+	if (k3NumList.length == 0) {
+		initChoiceList();
+	}
+}
+//删除不符合条件的号码
+function removeK3Num(typeResult, typeChoiceListTemp, i,choiceDetailIndex) {
+	if (choiceDetail[choiceDetailIndex] != -1 && choiceDetail[choiceDetailIndex] != typeResult) {// 过滤掉不符合规则的号码
+		k3NumList.splice(i, 1);
+		return false;
+	} else  {
+		return true;
+	} 
 }
 
-function drawPageAll(){
-	// 画胆拖
-	drawPage("d",danChoiceList);
-	drawPage("t",tuoChoiceList);
-	// 画类型
-	drawPage("lx",typeChoiceList);
-	//画和值
-	drawPage("hz",sumChoiceList);
-	// 画跨度
-	drawPage("kd",spanChoiceList);
-	//画奇偶
-	drawPage("jo",odd_evenChoiceList);
-	//画012
-	drawPage("012",lyeChoiceList);
-	//画大中小
-	drawPage("dzx",dzxChoiceList);
-}
-function drawPage(tagName,choiceList){
-	for(var i=0;i<choiceList.length;i++){
-		if(choiceList[i] == 0){
-			$("#" + tagName + i).attr("class","");
-		}else if(choiceList[i] == 1){
-			$("#" + tagName + i).attr("class","active");
-		}else if(choiceList[i] == 2){
-			$("#" + tagName + i).attr("class","disable");
+// 设置号码可选择情况
+function setChoiceStatus(choiceList, choiceListTemp) {
+	for (var i = 0; i < choiceListTemp.length; i++) {
+		if (choiceListTemp[i] == 2) {
+			choiceList[i] = 2;
+		} else if (choiceListTemp[i] == 0) {
+			if (choiceList[i] == 2) {
+				choiceList[i] = 0;
+			}
 		}
-		
+	}
+}
+
+function drawPageAll() {
+	// 画胆拖
+	drawPage("d", danChoiceList);
+	drawPage("t", tuoChoiceList);
+	// 画类型
+	drawPage("lx", typeChoiceList);
+	// 画和值
+	drawPage("hz", sumChoiceList);
+	// 画跨度
+	drawPage("kd", spanChoiceList);
+	// 画奇偶
+	drawPage("jo", odd_evenChoiceList);
+	// 画012
+	drawPage("012", lyeChoiceList);
+	// 画大中小
+	drawPage("dzx", dzxChoiceList);
+}
+function drawPage(tagName, choiceList) {
+	for (var i = 0; i < choiceList.length; i++) {
+		if (choiceList[i] == 0) {
+			$("#" + tagName + i).attr("class", "");
+		} else if (choiceList[i] == 1) {
+			$("#" + tagName + i).attr("class", "active");
+		} else if (choiceList[i] == 2) {
+			$("#" + tagName + i).attr("class", "disable");
+		}
+
 	}
 }
--- bc-wap/bc-wap/target/static/js/filter/k3Util.js
+++ bc-wap/bc-wap/target/static/js/filter/k3Util.js
@@ -1,23 +1,159 @@
 /**
- * 根据号码计算形态
- * 
- * @param k3Num
- * @return
+ * 根据号码计算形态数组下标
  */
 function calculateType(k3Num,typeChoiceListTemp){
-	for(var i=0;i<k3NumList.length;i++){
-		var k3Num = k3NumList[i]
 		if(k3Num.one - k3Num.three == 0){// 三同号
-			typeChoiceListTemp[2] = 0;
+			return 2;
 	    } else if(k3Num.two - k3Num.three == 0 || k3Num.two - k3Num.one == 0){// 二不同
-	    	typeChoiceListTemp[0] = 0;
+	    	return 0;
 	    }else{
 	        if(k3Num.three - k3Num.two == 1 && k3Num.two - k3Num.one == 1){// 三连号
-	        	typeChoiceListTemp[3] = 0;
+	        	return 3
 	        }
 	        // 三不同
-	        typeChoiceListTemp[1] = 0;
+	        return 1;
 	    }
+}
+
+/**
+ * 计算和值数组下标
+ */
+function calculateSum(k3Num,sumChoiceListTemp){
+	return k3Num.one + k3Num.two + k3Num.three - 3;
+}
+
+/**
+ * 计算跨度数组下标
+ */
+function  calculateSpan(k3Num){
+    return k3Num.three - k3Num.one;
+}
+
+/**
+ * 计算奇偶数组下标
+ */
+function   calculateOddEven(k3Num){
+    var odd = 0;
+    if(k3Num.one % 2 == 1){
+        ++ odd;
+    }
+    if(k3Num.two % 2 == 1){
+        ++odd;
+    }
+    if(k3Num.three % 2 == 1){
+        ++odd;
+    }
+    switch(odd){
+        case 0:
+            result = "1";
+            break;
+        case 1:
+            result = "3";
+            break;
+        case 2:
+            result = "2";
+            break;
+        case 3:
+            result = "0";
+            break;
+    }
+    return result;
+}
+
+/**
+ * 计算012路 
+ */
+function calculate012(k3Num){
+	var result = 0;
+    var lye = new K3Num(k3Num.one % 3,k3Num.two % 3, k3Num.three % 3);
+    if(lye.one == 0){
+    	if(lye.two == 0){
+    		if(lye.three == 0){
+    			result = 0;
+    		}else if(lye.three == 1){
+    			result = 1;
+    		}else if(lye.three == 2){
+    			result = 2;
+    		}
+    	}else if(lye.two == 1){
+    		if(lye.three == 1){
+    			result = 3;
+    		}else if(lye.three == 2){
+    			result = 4;
+    		}
+    	}else if(lye.two == 2){
+    		if(lye.three == 2){
+    			result = 5;
+    		}
+    	}
+    }else if(lye.one == 1){
+    	if(lye.two == 1){
+    		if(lye.three == 1){
+    			result = 6;
+    		}else if(lye.three == 2){
+    			result = 7;
+    		}
+    	}else if(lye.two == 2){
+    		if(lye.three == 2){
+    			result = 8;
+    		}
+    	}
+    }else if(lye.one == 2){
+    	if(lye.two == 2){
+    		if(lye.three == 2){
+    			result = 9;
+    		}
+    	}
+    }
+    return result;
+}
+
+/**
+ * 计算大中小
+ */
+function  calculateBigMidSmall(k3Num){
+	var result = 0;
+	
+	if(k3Num.one <= 2){
+		if(k3Num.two <= 2){
+			if(k3Num.three <= 2){
+				result = 3;
+			}else if(k3Num.three <= 4){
+				result = 4;
+			}else if(k3Num.three <= 6){
+				result = 5;
+			}
+		}else if(k3Num.two <= 4){
+			if(k3Num.three <= 4){
+				result = 6;
+			}else if(k3Num.three <= 6){
+				result = 0;
+			}
+		}else if(k3Num.two <= 6){
+			if(k3Num.three <= 6){
+				result = 7;
+			}
+		}
+	}else if(k3Num.one <= 4){
+		if(k3Num.two <= 4){
+			if(k3Num.three <= 4){
+				result = 2;
+			}else if(k3Num.three <= 6){
+				result = 8;
+			}
+		}else if(k3Num.two <= 6){
+			if(k3Num.three <= 6){
+				result = 9;
+			}
+		}
+	}else if(k3Num.one <= 6){
+		if(k3Num.two <= 6){
+			if(k3Num.three <= 6){
+				result = 1;
+			}
+		}
 	}
+	
+	return result;
 }
 
